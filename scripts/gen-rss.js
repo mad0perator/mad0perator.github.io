f7a549c77e482bcf97367b82cdbdb84df5dc8595
mad0perator <mad0perator.creationlounge@gmail.com>
2022-03-04 06:05:19 -0500

    Configured rss feed with `gen-rss.js`.

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'mad0perator',
+    site_url: 'https://mad0perator.github.io',
+    feed_url: 'https://mad0perator.github.io/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
