ba061491d9ddacb405e1baa0721fcd848462c1fc
mad0perator <mad0perator.creationlounge@gmail.com>
2022-03-04 06:08:12 -0500

    Configured meta in `pages/_document.js`

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'mad0perator | github',
+    description: 'Github landing page for mad0perator',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
